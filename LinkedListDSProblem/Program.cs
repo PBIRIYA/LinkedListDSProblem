1b40363364b8774b5d6ebd58bc99874d1a332572
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-21 21:44:14 +0530

    [Priya] add . adding Before Head

--- LinkedListDSProblem/Program.cs
+++ LinkedListDSProblem/Program.cs
@@ -12,6 +12,12 @@ namespace LinkedListDSProblem
             list.Add(56);
             list.Add(30);
             list.Add(70);
+            Console.WriteLine("\n=================================================================");
+            LinkedList list2 = new LinkedList();
+            list2.Add(70);
+            list2.AddBefore(30);
+            list2.AddBefore(56);
+            list.Display();
         }
     }
 }
